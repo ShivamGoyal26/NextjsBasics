600f8d7558bb27eb5831b337a7606007e2440628
Shivam Goyal <shivam@192.168.1.4>
2023-05-27 18:11:37 +0530

    Getting started

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1>About US</h1>
+      <Link href={"/"}>Link to home</Link>
+    </>
+  );
+};
+
+export default About;
